964da2994080de15ed9db7a0d5aaca2e2941f059
acanois <david.richter42@gmail.com>
2025-06-24 16:13:49 -0500

    add index buffer

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -8,18 +8,28 @@ use crate::vertex::Vertex;
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // A
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
-    },
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // B
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
-    },
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // E
 ];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 pub struct State {
     surface: wgpu::Surface<'static>,
@@ -30,7 +40,8 @@ pub struct State {
     pub window: Arc<Window>,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    index_buffer: wgpu::Buffer, 
+    num_indices: u32,
 }
 
 impl State {
@@ -165,7 +176,17 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
-        let num_vertices = VERTICES.len() as u32;
+        // Index buffer
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+
+        // Replaced num_vertices
+        let num_indices = INDICES.len() as u32;
 
         Ok(Self {
             surface,
@@ -176,7 +197,8 @@ impl State {
             render_pipeline,
             window,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indices,
         })
     }
 
@@ -239,9 +261,18 @@ impl State {
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
-            render_pass.set_pipeline(&self.render_pipeline); // 2.
-            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..)); // 3.
-            render_pass.draw(0..self.num_vertices, 0..1); // 4.
+
+            // Set pipeline
+            render_pass.set_pipeline(&self.render_pipeline);
+
+            // Set vertex buffer
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+
+            // Set index buffer
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+
+            // Draw indices
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
